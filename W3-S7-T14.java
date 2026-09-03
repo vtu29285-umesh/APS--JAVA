946100a245514e7a693d2ab24268b7ee27bd8bf1
vtu29285-umesh <vtu29285@veltech.edu.in>
2026-09-03 09:47:16 +0530

    Create W3-S7-T14.java

---
+++ W3-S7-T14.java
@@ -0,0 +1,34 @@
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+
+        PriorityQueue<ListNode> pq =
+            new PriorityQueue<>((a, b) -> a.val - b.val);
+
+        // Add the first node of every list
+        for (ListNode list : lists) {
+            if (list != null) {
+                pq.add(list);
+            }
+        }
+
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+
+        while (!pq.isEmpty()) {
+
+            // Get the smallest node
+            ListNode node = pq.poll();
+
+            // Add it to the result
+            current.next = node;
+            current = current.next;
+
+            // Add the next node from the same list
+            if (node.next != null) {
+                pq.add(node.next);
+            }
+        }
+
+        return dummy.next;
+    }
+}

6bdbbfeb4778ce79d8d7edb5e044eca4858a8ddd
vtu29285-umesh <vtu29285@veltech.edu.in>
2026-09-03 09:45:14 +0530

    Create W3-S7-T13.java

---
+++ W3-S7-T13.java
@@ -0,0 +1,30 @@
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+
+        // Dummy node to make handling the head easier
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+
+        while (list1 != null && list2 != null) {
+
+            if (list1.val <= list2.val) {
+                current.next = list1;
+                list1 = list1.next;
+            } else {
+                current.next = list2;
+                list2 = list2.next;
+            }
+
+            current = current.next;
+        }
+
+        // Attach whichever list still has nodes
+        if (list1 != null) {
+            current.next = list1;
+        } else {
+            current.next = list2;
+        }
+
+        return dummy.next;
+    }
+}
